4ade025991b1bd1697fdb22113fa70573d1ee6c9
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-30 17:59:57 -0400

    used fs.appendFileSync()

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js');
+fs.writeFileSync('notes.txt', 'My name is Kristen.');
+fs.appendFileSync('notes.txt', ' This is appended.');
+// Challenge: Append a message to notes.txt
+
+// 1. Use appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
